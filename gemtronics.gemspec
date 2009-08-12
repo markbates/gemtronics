1b7213726e7aef3b912d03f7bf16a64af95767fe
Mark Bates <mark@markbates.com>
2009-08-12 18:25:09 -0400

    Added alias_group functionality. load now has a default path.

--- gemtronics.gemspec
+++ gemtronics.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{gemtronics}
-  s.version = "0.0.1.20090812180550"
+  s.version = "0.0.1.20090812182507"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["markbates"]
--- lib/gemtronics/manager.rb
+++ lib/gemtronics/manager.rb
@@ -13,21 +13,27 @@ module Gemtronics
     def group(name, options = {})
       name = name.to_sym
       options = GLOBAL_DEFAULT_OPTIONS.merge(options)
-      yield self.groups[name] ||= Gemtronics::Grouper.new(options)
+      g = (self.groups[name] ||= Gemtronics::Grouper.new(options))
+      yield g if block_given?
     end
     
-    def load(file)
+    def alias_group(name, src)
+      group(name, :dependencies => src)
+    end
+    
+    def load(file = File.join(FileUtils.pwd, 'config', 'gemtronics.rb'))
       eval(File.read(file), binding)
     end
     
-    def require_gems(group = :default)
+    def require_gems(group = :default, options = {})
       group = self.groups[group.to_sym]
       return if group.nil?
-      
+      options = {:verbose => false}.merge(options)
       group.gems.each do |g|
         if g[:load] == true
           gem(g[:name], g[:version])
           g[:require].each do |f|
+            puts "require #{f}" if options[:verbose]
             require f
           end
         end
